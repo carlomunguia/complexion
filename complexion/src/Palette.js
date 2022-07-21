bdf6f967b044ca7ef7001eab211e4f33af982820
Carlo Munguia <cmunguia@denimgroup.com>
2022-07-20 22:29:21 -0500

    Complexion 5
    created a slider component, with styled css for the slider & using the rc-slider package to bring in its own props into Palette.js to adjust handle/rail/track style

--- complexion/src/Palette.js
+++ complexion/src/Palette.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import "rc-slider/assets/index.css";
 import "./Palette.css";
+import Slider from "rc-slider";
 
 class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+  changeLevel(level) {
+    this.setState({ level });
+  }
   render() {
-    const colorBoxes = this.props.colors.map((color) => (
-      <ColorBox background={color.color} name={color.name} />
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
     ));
     return (
       <div className='Palette'>
+        <div className='slider'>
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+            trackStyle={{ backgroundColor: "transparent" }}
+            railStyle={{ height: "8px" }}
+            handleStyle={{
+              backgroundColor: "turquoise",
+              outline: "none",
+              border: "2px solid turquoise",
+              boxShadow: "none",
+              width: "13px",
+              height: "13px",
+              marginLeft: "-7px",
+              marginTop: "-3px",
+            }}
+          />
+        </div>
         <div className='Palette-colors'>{colorBoxes}</div>
       </div>
     );
