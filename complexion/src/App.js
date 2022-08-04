882a7ff0ded8005f172e430dce72dcc78c43ad8d
Carlo Munguia <cmunguia@denimgroup.com>
2022-08-03 23:11:29 -0500

    Complexion 14
    added link to color box to work with a new single color palette

--- complexion/src/App.js
+++ complexion/src/App.js
@@ -12,8 +12,11 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<PaletteList palettes={seedColors} />} />
-      <Route path='/palette/:paletteId' element={<Palette />} /> //no more
-      palette prop
+      <Route path='/palette/:paletteId' element={<Palette />} />
+      <Route
+        path='/palette/:paletteId/:colorId'
+        element={<h1> SINGLE COLORS HERE</h1>}
+      />
     </Routes>
   );
 }
